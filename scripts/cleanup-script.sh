0b3b0fe4e8a52f0bd1aced86dd90320cf77bd30e
nvtuluva <nvtuluva@sysgain.com>
2019-04-23 18:31:00 +0530

    Update cleanup-script.sh

--- scripts/cleanup-script.sh
+++ scripts/cleanup-script.sh
@@ -1,16 +1,12 @@
-#!/bin/bash
-
+#! /bin/bash
 deletevm=$1
 deletenic=$2
 deletensg=$3
-deletepip=$4
-deletevnet=$5
-deletedisk=$6
-deleteroutetable=$7
-deleteroute=$8
-rgName=$9
-
-
+deletevnet=$4
+deletedisk=$5
+deleteroutetable=$6
+deleteroute=$7
+rgName=$8
 #################################################
 
 #Deleting the existing workload vm 
@@ -18,17 +14,9 @@ rgName=$9
 #################################################
 
 az vm delete -g $rgName -n $deletevm --yes
-
 az network nic delete -g $rgName -n $deletenic 
-
 az network nsg delete -g $rgName -n $deletensg 
-
-az network public-ip delete -g $rgName -n $deletepip 
-
 az network vnet delete -g $rgName -n $deletevnet 
-
 az disk delete --name $deletedisk --resource-group $rgName
-
 az network route-table route delete -g $rgName --route-table-name $deleteroutetable -n $deleteroute 
-
 az network route-table delete -g $rgName -n $deleteroutetable
