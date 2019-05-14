c70920bcdf946fbcccd35fed6ec5d4408e20fd83
nvtuluva <nvtuluva@sysgain.com>
2019-05-14 11:21:00 +0530

    Update fg-python-script.sh

--- scripts/fg-python-script.sh
+++ scripts/fg-python-script.sh
@@ -18,33 +18,29 @@ echo "NSG=${13}" >>outparams.txt
 echo "RouteTable=${14}" >>outparams.txt
 echo "Route=${15}" >>outparams.txt
 echo "VNet=${16}" >>outparams.txt
+subnetname1=${17}
+subnetname2=${18}
+subnetaddprefix1=${19}
+subnetaddprefix2=${20}
 
-
-#download the python files to the home dir
+#download all the pre-requsites to install python3 and all the python files to the home dir
 
 sudo apt-get -y update
-
 sudo apt-get -y upgrade
-
 sudo add-apt-repository ppa:jonathonf/python-3.6 -y 
-
 sudo apt-get update -y
-
 sudo apt-get install python3.6 -y
-
 sudo apt-get install -y python3-pip
-
-pip3 install requests 
-
+sudo pip3 install requests 
 sudo apt-get install build-essential libssl-dev libffi-dev python-dev -y
-
 sudo apt-get install -y python3-venv
-
-python3 -m venv my_env
-
+sudo python3 -m venv my_env
 source my_env/bin/activate
 
 cd /home/$azureuser
 
-#wget "cleanupscript"
-#wget "python script"
+wget "https://storageccqia.blob.core.windows.net/cc-iot/fortigate/fortigate-azure/scripts/cleanup.sh"
+wget "https://storageccqia.blob.core.windows.net/cc-iot/fortigate/fortigate-azure/scripts/cleanupSpokeVnet.py"
+wget "https://storageccqia.blob.core.windows.net/cc-iot/fortigate/fortigate-azure/scripts/enableRoute.py"
+
+python3 enableRoute.py $AppID $AppPassword $TenantID $SubscriptionID $RGName ${VNet} ${subnetname1} ${subnetname2} ${subnetaddprefix1} ${subnetaddprefix2} ${Route}
